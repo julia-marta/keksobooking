3af5fa871aeee69b4705ca84085e07bdf0c2a289
julia-marta <marta237@yandex.ru>
2020-06-19 21:41:58 +0300

    Добавляет универсальную функцию обработки данных

--- js/upload.js
+++ js/upload.js
@@ -2,26 +2,19 @@
 (function () {
   var URL_TO_GET = 'https://javascript.pages.academy/keksobooking/data';
   var URL_TO_POST = 'https://javascript.pages.academy/keksobooking';
+  var GET = 'GET';
+  var POST = 'POST';
 
   var statusCode = {
     OK: 200
   };
 
-  var getData = function (onLoad) {
+  var onData = function (method, url, data, onLoad, onError) {
+
     var xhr = new XMLHttpRequest();
     xhr.responseType = 'json';
-
-    xhr.addEventListener('load', function () {
-      onLoad(xhr.response);
-    });
-
-    xhr.open('GET', URL_TO_GET);
-    xhr.send();
-  };
-
-  var sendData = function (data, onLoad, onError) {
-    var xhr = new XMLHttpRequest();
-    xhr.responseType = 'json';
+    xhr.open(method, url);
+    xhr.send(data);
 
     xhr.addEventListener('load', function () {
       if (xhr.status === statusCode.OK) {
@@ -33,13 +26,14 @@
     xhr.addEventListener('error', function () {
       onError();
     });
-
-    xhr.open('POST', URL_TO_POST);
-    xhr.send(data);
   };
 
   window.upload = {
-    get: getData,
-    send: sendData
+    get: function (onLoad) {
+      onData(GET, URL_TO_GET, null, onLoad, null);
+    },
+    send: function (data, onLoad, onError) {
+      onData(POST, URL_TO_POST, data, onLoad, onError);
+    }
   };
 })();
