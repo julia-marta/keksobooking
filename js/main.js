3146dab42590a9adba14d3c3fd84a702294931ee
julia-marta <marta237@yandex.ru>
2020-07-06 18:17:32 +0300

    Удаляет неиспользуемую функцию

--- js/main.js
+++ js/main.js
@@ -24,17 +24,12 @@
     }
   };
 
-  var getRandomInt = function (min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-  };
-
   window.main = {
     page: mainPage,
     map: map,
     form: form,
     filter: filter,
     isEnterEvent: isEnterEvent,
-    isEscEvent: isEscEvent,
-    getRandomInt: getRandomInt
+    isEscEvent: isEscEvent
   };
 })();
